3e815af26a16e2c9545f33727ac1b0e33da10a4a
ibrahimkhalilhemu <ibrahimkhalilhemu1@gmail.com>
2021-05-10 15:55:57 +0600

    plus button and min button add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import { useDispatch, useSelector } from 'react-redux';
+import { decrease, increase } from './actions';
 import './App.css';
 
 function App() {
+  const btnState = useSelector((state)=>state.changeNumber);
+  const dispatch = useDispatch();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Welcome my redux first project</h1>
+     <div className="quantity">
+        <a className="quantity__minus" title="Decrement"
+        onClick={()=>dispatch(decrease())}
+        ><span>-</span></a>
+        <input className="quantity" type="text" className="quantity__input" value={btnState} />
+        <a className="quantity__plus" title="Increment"
+         onClick={()=>dispatch(increase())}
+        ><span>+</span></a>
+      </div>
+  
     </div>
   );
 }
---
+++ src/reducres/increAndDrec.js
@@ -0,0 +1,11 @@
+const initialState = 0;
+
+const changeNumber = (state=initialState , action)=>{
+    switch (action.type){
+        case "INCREASE" : return state + 1;
+        case "DECREASE" : return state - 1;
+        default : return state;
+    }
+}
+
+export default changeNumber;
